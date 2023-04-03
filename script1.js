99457cab31d8543f859f37a403a2e19da872fa6f
MARCIO PASOLD JÚNIOR <marcio_pasold-junior@estudante.sesisenai.org.br>
2023-04-03 08:01:52 -0300

    diferenciando constantes e variáveis

---
+++ script1.js
@@ -0,0 +1,24 @@
+const frase = "Anything" // constante => rótulo
+
+const PI = 3.1415927
+
+console.log(frase)
+console.log(PI)
+
+
+let texto = "Computação" // variável => não aceita spbrescrita
+console.log(texto)
+console.log('-------------------------')
+texto = "Computação"
+console.log(texto)
+console.log('-------------------------')
+var texto2 = "teste" // variável => aceita sobrescrita
+console.log(texto2)
+console.log('-------------------------')
+var texto2 = "teste2"
+console.log(texto2)
+console.log('-------------------------')
+
+texto3 = "teste3" // declaração feita pelo interpretador do JS
+console.log(texto3)
+console.log('-------------------------')
